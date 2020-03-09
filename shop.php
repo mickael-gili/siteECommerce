48364a216e2e00c7226d567d2741a9f105918165
lesly <lesly.mongin@isen-ouest.yncrea.fr>
2020-03-09 14:46:53 +0100

    amélioration detail.php

--- shop.php
+++ shop.php
@@ -52,72 +52,181 @@ include("fonctions/functions.php");
     
     ?>
                
-        </div><!-- col-md-3 Finish -->
+               </div><!-- col-md-3 Finish -->
            
            <div class="col-md-9"><!-- col-md-9 Begin -->
-                <?php
-                
-                if (!isset($_GET['p_cat'])){
+             
+             <?php 
+               
+                if(!isset($_GET['p_cat'])){
+                    
+                    if(!isset($_GET['cat'])){
+              
+                      echo "
 
-                    if (!isset($_GET['cat'])){
+                       <div class='box'><!-- box Begin -->
+                           <h1>Shop</h1>
+                           <p>
+                               Les meilleures huiles du marché à petits prix !
+                           </p>
+                       </div><!-- box Finish -->
 
-                        echo"
-                        <div class='box'><!-- box Begin -->
-                        <h1>Shop</h1>
-                        <p>
-                            Les meilleurs huiles du marché à petit prix!
-                        </p>
-                        </div><!-- box Finish -->
-                        ";
+                       ";
+                        
                     }
-                }
+                   
+                   }
                
                ?>
                
                <div class="row"><!-- row Begin -->
-
-               <?php
-                
-                if (!isset($_GET['p_cat'])){
-
-                    if (!isset($_GET['cat'])){
-
-
-                    }
-                }
                
-               ?>
+                   <?php 
+                   
+                        if(!isset($_GET['p_cat'])){
+                            
+                         if(!isset($_GET['cat'])){
+                            
+                            $per_page=6; 
+                             
+                            if(isset($_GET['page'])){
+                                
+                                $page = $_GET['page'];
+                                
+                            }else{
+                                
+                                $page=1;
+                                
+                            }
+                            
+                            $start_from = ($page-1) * $per_page;
+                             
+                            $get_products = "select * from products order by 1 DESC LIMIT $start_from,$per_page";
+                             
+                            $run_products = mysqli_query($con,$get_products);
+                             
+                            while($row_products=mysqli_fetch_array($run_products)){
+                                
+                                $pro_id = $row_products['product_id'];
+        
+                                $pro_title = $row_products['product_title'];
 
+                                $pro_price = $row_products['product_price'];
+
+                                $pro_img1 = $row_products['product_img1'];
+                                
+                                echo "
+                                
+                                    <div class='col-md-4 col-sm-6 center-responsive'>
+                                    
+                                        <div class='product'>
+                                        
+                                            <a href='details.php?pro_id=$pro_id'>
+                                            
+                                                <img class='img-responsive' src='admin_area/product_images/$pro_img1'>
+                                            
+                                            </a>
+                                            
+                                            <div class='text'>
+                                            
+                                                <h3>
+                                                
+                                                    <a href='details.php?pro_id=$pro_id'> $pro_title </a>
+                                                
+                                                </h3>
+                                            
+                                                <p class='price'>
+
+                                                    $pro_price €
+
+                                                </p>
+
+                                                <p class='buttons'>
+
+                                                    <a class='btn btn-default' href='details.php?pro_id=$pro_id'>
+
+                                                        View Details
+
+                                                    </a>
+
+                                                    <a class='btn btn-primary' href='details.php?pro_id=$pro_id'>
+
+                                                        <i class='fa fa-shopping-cart'></i> Add To Cart
+
+                                                    </a>
+
+                                                </p>
+                                            
+                                            </div>
+                                        
+                                        </div>
+                                    
+                                    </div>
+                                
+                                ";
+                                
+                        }
+                        
+                   ?>
+               
                </div><!-- row Finish -->
                
                <center>
-                   <ul class="pagination">
-
-                   <?php
-                
-                    if (!isset($_GET['p_cat'])){
-
-                        if (!isset($_GET['cat'])){
-
-                            $per_page=6;
-                            if(isset($_GET['page'])){
-                                $page = $_GET['page'];
-                                
-                                else{
-
-                                    $page=1;
-
-                                }
-                                $start_from = ($page=1)* $per_page;
-
-                            }
-                        }
-                    }
-                
-                    ?>
-
-                   </ul>
+                   <ul class="pagination"><!-- pagination Begin -->
+					 <?php
+                             
+                    $query = "select * from products";
+                             
+                    $result = mysqli_query($con,$query);
+                             
+                    $total_records = mysqli_num_rows($result);
+                             
+                    $total_pages = ceil($total_records / $per_page);
+                             
+                        echo "
+                        
+                            <li>
+                            
+                                <a href='shop.php?page=1'> ".'Première page'." </a>
+                            
+                            </li>
+                        
+                        ";
+                             
+                        for($i=1; $i<=$total_pages; $i++){
+                            
+                              echo "
+                        
+                            <li>
+                            
+                                <a href='shop.php?page=".$i."'> ".$i." </a>
+                            
+                            </li>
+                        
+                        ";  
+                            
+                        };
+                             
+                        echo "
+                        
+                            <li>
+                            
+                                <a href='shop.php?page=$total_pages'> ".'Dernière page'." </a>
+                            
+                            </li>
+                        
+                        ";
+                             
+					    	}
+							
+						}
+					 
+					 ?> 
+                       
+                   </ul><!-- pagination Finish -->
                </center>
+                
+                <?php getpcatpro(); ?>  
                
            </div><!-- col-md-9 Finish -->
            
